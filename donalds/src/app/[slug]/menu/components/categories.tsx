febf6a609db6eff4583fd5ce75c6e35035c4cdd1
Balbino-teste <balbinojteste@gmail.com>
2025-08-28 14:35:54 -0300

    Foi realizado ajustes para melhor visualização do código

--- donalds/src/app/[slug]/menu/components/categories.tsx
+++ donalds/src/app/[slug]/menu/components/categories.tsx
@@ -9,53 +9,71 @@ import { Button } from "@/components/ui/button"
 import { useState } from "react"
 
 interface RestaurantCategoriesProps {
-  restaurant: Prisma.RestaurantGetPayload<{
-    include: {
-        menuCategories: {
-            include:{products:true}
+    restaurant: Prisma.RestaurantGetPayload<{
+        include: {
+            menuCategories: {
+                include: { products: true }
+            }
         }
-    }
-  }>
+    }>
 }
 
 const RestaurantCategories = ({ restaurant }: RestaurantCategoriesProps) => {
-    const [selectedCategory, setSelectdCategory] = useState<MenuCategory>(restaurant.menuCategories|
-        const handleCategoryClick = (category: MenuCategory) =>{
-            setSelectdCategory(category)
-        })
-  return (  
+    const [selectedCategory, setSelectedCategory] = useState<MenuCategory>(
+        restaurant.menuCategories[0] // inicializa com a primeira categoria
+    )
+
+    const handleCategoryClick = (category: MenuCategory) => {
+        setSelectedCategory(category)
+    }
+
+    const getCategoryButtonVariant = (category: MenuCategory) => {
+        return selectedCategory?.id === category.id ? "default" : "secondary"
+}
+
+return (
     <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl border bg-white">
-       <div className="p-5">
-        <div className="flex items-center gap-3 ">
-        <Image 
-          src={restaurant.avatarImageUrl} 
-          alt={restaurant.name} 
-          height={45} 
-          width={45}
-          className="rounded-full object-cover"
-        />
-        <div>
-          <h2 className="font-semibold text-lg">{restaurant.name}</h2>
-          <p className="text-xs opacity-55">{restaurant.description}</p>
-        </div>      
-      </div>
-      <div className="mt-3 flex items-center gap-1 text-xs text-green-500 ">
-            <ClockIcon size={12} />
-            <p>Aberto!</p>
-        </div> 
-       </div>
-        <ScrollArea className="w-full mt-s">
+        <div className="p-5">
+            <div className="flex items-center gap-3 ">
+                <Image
+                    src={restaurant.avatarImageUrl}
+                    alt={restaurant.name}
+                    height={45}
+                    width={45}
+                    className="rounded-full object-cover"
+                />
+                <div>
+                    <h2 className="font-semibold text-lg">{restaurant.name}</h2>
+                    <p className="text-xs opacity-55">{restaurant.description}</p>
+                </div>
+            </div>
+            <div className="mt-3 flex items-center gap-1 text-xs text-green-500 ">
+                <ClockIcon size={12} />
+                <p>Aberto!</p>
+            </div>
+        </div>
+
+        {/* Scroll de categorias */}
+        <ScrollArea className="w-full mt-2">
             <div className="flex w-max space-x-4 p-4 pt-0">
-                {restaurant.menuCategories.map(category => (
-                    <Button onClick={() => handleCategoryClick(category)} key={category.id} variant="secondary" size="sm" className="rounded-full">
+                {restaurant.menuCategories.map((category) => (
+                    <Button
+                        onClick={() => handleCategoryClick(category)}
+                        key={category.id}
+                        variant={
+                            getCategoryButtonVariant(category)
+                         }
+                        size="sm"
+                        className="rounded-full"
+                    >
                         {category.name}
                     </Button>
                 ))}
             </div>
-            <ScrollBar orientation="horizontal"/>
+            <ScrollBar orientation="horizontal" />
         </ScrollArea>
     </div>
-  )
+)
 }
 
 export default RestaurantCategories
